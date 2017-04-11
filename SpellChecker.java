ffb665b1c78eda62ab4d9f8e98494578132213b4
raviteja4c3 <raviteja4c3@gmail.com>
2017-04-11 15:21:35 +0530

    Updated SpellChecker

--- SpellChecker.java
+++ SpellChecker.java
@@ -1,1 +1,14 @@
 
+package com.tpoint;
+
+public class SpellChecker {
+	
+	
+	public SpellChecker(){
+		System.out.println("Inside SpellCheker Constructor..");   //syso
+	}
+	public void checkSpelling(){
+	      System.out.println("Inside checkSpelling." );
+	 }
+
+}
